b5f85e5685a10c9091d41d12b96431f8b380a2c4
Jorge André Alves <jorgeandrealves@gmail.com>
2018-03-06 18:48:31 +0000

    :hammer: base Web3 and JS interactions migrated from private repo

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app id="app">
+    <v-layout justify-center>
+      <v-flex xs12 sm8 md6 lg4>
+        <v-card>
+          <v-card-text>
+            <KeyStoreAuthForm></KeyStoreAuthForm>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+
+    <router-view/>
+  </v-app>
+</template>
+
+<script>
+import KeyStoreAuthForm from '@/components/KeyStoreAuthForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'KeyStoreAuthForm': KeyStoreAuthForm
+  }
+}
+</script>
