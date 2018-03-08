50bae433aec36c11beccb120c0ec0ca1e7c3eba4
Jorge André Alves <jorgeandrealves@gmail.com>
2018-03-08 14:46:44 +0000

    wip

--- src/components/TransactionForm.vue
+++ src/components/TransactionForm.vue
@@ -1,23 +1,29 @@
 <template>
   <!-- SHAME: useless div is just here because the template must have a single child -->
   <div>
-    <form action="#" enctype="multipart/form-data" @submit="submitForm">
+    <form action='#' enctype='multipart/form-data' @submit='submitForm'>
       <v-text-field
-        name="amount"
-        label="Amount"
-        v-model="amount"
-        type="number"
+        name='amount'
+        label='Amount'
+        v-model='amount'
+        type='number'
       ></v-text-field>
       <v-text-field
-        name="receiver"
-        label="Receiver"
-        v-model="receiver"
-        type="text"
+        name='receiver'
+        label='Receiver'
+        v-model='receiver'
+        type='text'
       ></v-text-field>
-      <div v-if="receiver"> Receiver: {{receiver}} </div>
-      <div v-if="amount"> Amount ({{ethereumCurrency}}): {{amount}} </div>
-      <div v-if="ethereumValue"> (in Ethereum): {{ethereumValue}} </div>
-      <v-btn type="submit">submit</v-btn>
+      <v-text-field
+        name='gas'
+        label='Gas'
+        v-model='gas'
+        type='text'
+      ></v-text-field>
+      <div v-if='receiver'> Receiver: {{receiver}} </div>
+      <div v-if='amount'> Amount ({{ethereumCurrency}}): {{amount}} </div>
+      <div v-if='ethereumValue'> (in Ethereum): {{ethereumValue}} </div>
+      <v-btn type='submit'>submit</v-btn>
     </form>
   </div>
 </template>
@@ -34,18 +40,53 @@ export default {
   },
   computed: {
     ethereumValue: function () {
-      return this.amount ? (this.amount / this.$store.state.currencyValue) : 0
+      return this.amount ? this.amount / this.$store.state.currencyValue : 0
     },
     ethereumCurrency: function () {
       return this.$store.state.currency
+    },
+    gasPrice: function () {
+      return 123456
     }
   },
   methods: {
-    processTransaction: function (keyStoreData) {
+    processTransaction: function () {
+      const web3 = this.$store.state.web3
+      let txObject = {
+        from: this.$store.state.walletAddress,
+        to: this.receiver,
+        amount: this.ethereumValue,
+        gas: this.gas,
+        gasPrice: this.gasPrice
+      }
 
+      console.log(this.$store.state.privateKey)
+      web3.eth.signTransaction(txObject, this.$store.state.privateKey).then(
+        web3.eth.sendSignedTransaction
+      )
+
+      // web3.eth
+      //   .sendTransaction(txObject)
+      //   .once('transactionHash', hash => {
+      //     console.log('Tansaction hashed')
+      //   })
+      //   .once('receipt', receipt => {
+      //     console.log('reciept')
+      //   })
+      //   .on('confirmation', (confNumber, receipt) => {
+      //     console.log('Confirmed')
+      //   })
+      //   .on('error', function (error) {
+      //     console.log(error)
+      //   })
+      //   .then(function (receipt) {
+      //     // will be fired once the receipt its mined
+      //     console.log('mined')
+      //   })
     },
     submitForm: function (e) {
       e.preventDefault()
+      this.processTransaction()
     }
   }
 }
--- src/helpers/gas.js
+++ src/helpers/gas.js
@@ -1,13 +1,7 @@
-import Web3 from 'web3'
+import web3 from 'web3'
 
-let web3
-
-var currentGasPrice = () => {
-    let gasPrice = web3.eth.gasPrice;
-    return gasPrice;
-}
-
-module.exports = {
-	currentGasPrice, // similar to addNote: addNote, using ES6 syntax (key and value are the same)
-
+export default {
+  currentGasPrice: () => {
+    return web3.eth.gasPrice
+  }
 }
